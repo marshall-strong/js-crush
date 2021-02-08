a39c7740d6685daaaec51c4f8d72d18fb74176b7
marshall-strong <marshallstrong123@gmail.com>
2021-02-08 12:14:36 -0500

    refactor Gem

--- src/gem.js
+++ src/gem.js
@@ -1,24 +1,28 @@
 const Gem = function (letter, id) {
-  // immutable data descriptors
+  ////////////////////////////////////////////////
+  // GEM PROPERTIES
+
   Object.defineProperty(this, "letter", {
     enumberable: false,
     configurable: false,
     writable: false,
     value: letter,
   });
+
   Object.defineProperty(this, "id", {
     enumberable: false,
     configurable: false,
     writable: false,
     value: id,
   });
-  // mutable data descriptors
+
   Object.defineProperty(this, "row", {
     enumberable: false,
     configurable: true,
     writable: true,
     value: null,
   });
+
   Object.defineProperty(this, "col", {
     enumberable: false,
     configurable: true,
