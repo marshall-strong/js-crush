fbbcd303918dff39094f00443107661f1b19c613
marshall-strong <marshallstrong123@gmail.com>
2021-02-09 12:43:34 -0500

    rename `ctx2` -> `ctxt`

--- src/buttons.js
+++ src/buttons.js
@@ -11,78 +11,78 @@ $(document).on("click", "#getHint", function (event) {
   const helpMove = game.getRandomValidMove();
   const cellSize = 600 / board.dimension;
   const arrowLength = cellSize / 4;
-  var canvas = document.getElementById("gameCanvas");
-  var ctx2 = canvas.getContext("2d");
-  ctx2.beginPath();
+  const canvas = document.getElementById("gameCanvas");
+  const ctxt = canvas.getContext("2d");
+  ctxt.beginPath();
   posY = (helpMove.gem.row + 1) * cellSize;
   posX = helpMove.gem.col * cellSize;
-  ctx2.strokeStyle = "black";
+  ctxt.strokeStyle = "black";
 
   switch (helpMove.direction) {
     case "right":
       $("#mainColumn").html(game.drawBoard());
       posX = posX + cellSize * 1.2;
       posY = posY - cellSize / 2;
-      ctx2.moveTo(posX, posY);
-      ctx2.lineTo(posX - arrowLength, posY - arrowLength);
-      ctx2.lineTo(posX - arrowLength, posY + arrowLength);
-      ctx2.fill();
-      ctx2.rect(
+      ctxt.moveTo(posX, posY);
+      ctxt.lineTo(posX - arrowLength, posY - arrowLength);
+      ctxt.lineTo(posX - arrowLength, posY + arrowLength);
+      ctxt.fill();
+      ctxt.rect(
         posX - arrowLength * 2,
         posY - arrowLength / 2,
         arrowLength,
         arrowLength
       );
-      ctx2.fill();
+      ctxt.fill();
       break;
     case "left":
       $("#mainColumn").html(game.drawBoard());
       posX = posX - cellSize / 4;
       posY = posY - cellSize / 2;
-      ctx2.moveTo(posX, posY);
-      ctx2.lineTo(posX + arrowLength, posY + arrowLength);
-      ctx2.lineTo(posX + arrowLength, posY - arrowLength);
-      ctx2.fill();
-      ctx2.rect(
+      ctxt.moveTo(posX, posY);
+      ctxt.lineTo(posX + arrowLength, posY + arrowLength);
+      ctxt.lineTo(posX + arrowLength, posY - arrowLength);
+      ctxt.fill();
+      ctxt.rect(
         posX + arrowLength,
         posY - arrowLength / 2,
         arrowLength,
         arrowLength
       );
-      ctx2.fill();
+      ctxt.fill();
       break;
     case "up":
       $("#mainColumn").html(game.drawBoard());
       posY = posY - cellSize * 1.2;
       posX = posX + cellSize / 2;
-      ctx2.moveTo(posX, posY);
-      ctx2.lineTo(posX - arrowLength, posY + arrowLength);
-      ctx2.lineTo(posX + arrowLength, posY + arrowLength);
-      ctx2.fill();
-      ctx2.rect(
+      ctxt.moveTo(posX, posY);
+      ctxt.lineTo(posX - arrowLength, posY + arrowLength);
+      ctxt.lineTo(posX + arrowLength, posY + arrowLength);
+      ctxt.fill();
+      ctxt.rect(
         posX - arrowLength / 2,
         posY + arrowLength,
         arrowLength,
         arrowLength
       );
-      ctx2.fill();
+      ctxt.fill();
       break;
     case "down":
       $("#mainColumn").html(game.drawBoard());
       posY = posY + cellSize / 4;
       posX = posX + cellSize / 2;
-      ctx2.moveTo(posX, posY);
-      ctx2.lineTo(posX + arrowLength, posY - arrowLength);
-      ctx2.lineTo(posX - arrowLength, posY - arrowLength);
-      ctx2.fill();
-      ctx2.rect(
+      ctxt.moveTo(posX, posY);
+      ctxt.lineTo(posX + arrowLength, posY - arrowLength);
+      ctxt.lineTo(posX - arrowLength, posY - arrowLength);
+      ctxt.fill();
+      ctxt.rect(
         posX - arrowLength / 2,
         posY - arrowLength * 2,
         arrowLength,
         arrowLength
       );
-      ctx2.fill();
+      ctxt.fill();
       break;
   }
-  ctx2.closePath();
+  ctxt.closePath();
 });
