0783e9b0fec6241766caf2d817c100a949ec0d1f
marshall-strong <marshallstrong123@gmail.com>
2021-02-11 11:10:18 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 // Seed RNG to generate initial board
 Math.seedrandom(0);
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-const col_array = alphabet.split("");
-
 const defaultSize = 8;
 const board = new Board(defaultSize);
 const game = new Game(board);
 
 // width and height (in pixels) of the gameCanvas
+// Need to make the canvas size responsive using CSS,
+// then somehow get the size here to use in later calculations...
 const gameCanvasSize = 600;
-
-// game.setupGameboard();
-// game.drawBoard();
-// board.resetScore();
 
 // update score display when score value changes
 $(board).on("scoreUpdate", function (e, info) {
