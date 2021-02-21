6cd2de6744fc7f39919f022d3db88d23d9be99da
marshall-strong <marshallstrong123@gmail.com>
2021-02-21 02:47:35 -0500

    add disjoint-set implementation: `makeSet()`, `find(node)`, `union(node1, node2)`

--- javascript/disjoint-set.js
+++ javascript/disjoint-set.js
@@ -14,12 +14,81 @@
 // To distinguish root nodes from others, their parent pointers have invalid values, such as a circular reference to the node, or a sentinel value.
 // Each tree represents a set stored in the forest, with the members of the set as the nodes in the tree.
 // Root nodes provide set representatives: two nodes are in the same set if and only if the roots of the trees containing the nodes are equal.
+
 // Nodes in the forest can be stored in any way convenient to the application, but a common technique is to store them in an array.
 // In an array, parents can be indicated by their array index.
 // Every array entry requires a minimum of O(lg n) bits of storage for the parent pointer.
 // A comparable or lesser amount of storage is required for the rest of the entry, so the number of bits required to store the forest is  O(n lg n).
 // If an implementation uses fixed size nodes (thereby limiting the maximum size of the forest that can be stored), then the necessary storage is linear O(n).
 
-class DisjointSet {
-  constructor() {}
+// https://github.com/manubb/union-find
+
+// returns an object with two properties: `rank` and `parent`
+// add other properties during implementation
+// is `node` the root of a tree? `node.parent === node`
+export const makeSet = () => {
+  const node = { rank: 0 };
+  node.parent = node;
+  return node;
+};
+
+// returns the root of the tree containing `node`
+// are `node1` and `node2` in the same tree? `find(node1) === find(node2)`
+export const find = (node) => {
+  if (node.parent !== node) {
+    node.parent = find(node.parent);
+  }
+  return node.parent;
+};
+
+// merges the sets containing `node1` and `node2`
+// returns undefined
+export const union = (node1, node2) => {
+  const root1 = find(node1);
+  const root2 = find(node2);
+  if (root1 !== root2) {
+    if (root1.rank < root2.rank) {
+      root1.parent = root2;
+    } else {
+      root2.parent = root1;
+      if (root1.rank === root2.rank) root1.rank++;
+    }
+  }
+};
+
+class Forest {
+  constructor() {
+    this.unioned = {};
+    this.setSizes = {};
+  }
+
+  // Finds the set representative for the set that this key is a member of.
+  find(key) {
+    let parent = this.unioned[key];
+    if (parent === null) {
+      return key;
+    } else {
+      parent = this.find(parent);
+      this.unioned[key] = parent; // path compression
+      return parent;
+    }
+  }
+
+  // Returns the size of `set` (assumes 1 if not stored)
+  setSize(set) {
+    return this.setSizes[set] || 1;
+  }
+
+  union(key1, key2) {
+    let parent1 = this.find(key1);
+    let parent2 = this.find(key2);
+    if (parent1 === parent2) {
+      // the two keys are in the same set
+      return parent1;
+    } else {
+      this.unioned[parent2] = parent1;
+      this.setSizes[parent1] = setSize(parent1) + this.setSize(parent2);
+      delete this.setSizes[parent2];
+    }
+  }
 }
