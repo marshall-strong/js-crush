c42eb86dca79d9f7513f1401914d3a50f1078791
marshall-strong <marshallstrong123@gmail.com>
2021-02-20 22:59:38 -0500

    notes on Disjoint-Set data structures

---
+++ javascript/disjointSet.js
@@ -0,0 +1,11 @@
+// https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+
+// A Disjoint-Set is a data structure that stores a collection of disjoint (non-overlapping) sets.
+// It provides operations for the following:
+// -- adding new sets
+// -- merging sets (by replacing them with their union)
+// -- finding a representative member of a set (efficiently determine whether two elements are in the same set or not)
+
+class DisjointSet {
+  constructor() {}
+}
